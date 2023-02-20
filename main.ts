ac659c9e761c6acbc3f7fb914fe304ffd6729dfc
Daniel Gallichan <dgallichan@yahoo.com>
2023-02-20 16:06:06 +0000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,25 @@
+radio.onReceivedValue(function (name, value) {
+    if (name == "Red") {
+        valueRed = Math.constrain(value, 0, 255)
+    } else if (name == "Green") {
+        valueGreen = Math.constrain(value, 0, 255)
+    } else if (name == "Blue") {
+        valueBlue = Math.constrain(value, 0, 255)
+    } else if (name == "Bright") {
+        valueBrightness = Math.constrain(value, 0, 255)
+    }
+})
+let valueBrightness = 0
+let valueBlue = 0
+let valueGreen = 0
+let valueRed = 0
+radio.setGroup(200)
+let tileDisplay = Kitronik_Zip_Tile.createZIPTileDisplay(1, 1, Kitronik_Zip_Tile.UBitLocations.Visible)
+valueRed = 10
+valueGreen = 10
+valueBlue = 10
+valueBrightness = 50
+basic.forever(function () {
+    tileDisplay.showColor(Kitronik_Zip_Tile.rgb(valueRed, valueGreen, valueBlue))
+    tileDisplay.setBrightness(valueBrightness)
+})
